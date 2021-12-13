1fb0e481bf544c56c2c192ad40a192bfc595f335
Faik Turan <turanfaik@hotmail.com>
2021-12-13 17:09:41 +0300

    Add project files.

---
+++ MCD_SystemIOFileIslemleri/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MCD_SystemIOFileIslemleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //bool kontrol = File.Exists("c:\\NetworkAkademi\\merhaba.txt");
+            //if (kontrol)
+            //{
+            //    Console.WriteLine("Belirtmiş olduğunuz dosya sistem içerisinde bulunmaktadır.");
+            //}
+            //else
+            //{
+            //    FileCreate("c:\\NetworkAkademi\\merhaba.txt");
+            //}
+
+            Console.ReadLine();
+
+
+        }
+
+        static void FileCreate(string path)
+        {
+            FileStream Fs = File.Create(path);
+            Fs.Close();
+        }
+
+        static string FileReadAllText(string path)
+        {
+            string metinDegeri = File.ReadAllText(path);
+            return metinDegeri;
+        }
+
+
+    }
+}
